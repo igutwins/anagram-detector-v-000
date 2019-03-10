0db7b7ab3a6cdda72cabb3ccc80eb904a6308799
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-10 23:58:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
   end
   def match(some_words)
     some_words.find_all do |word|
-      if word.split("").sort == @word.split("").sort
+      if some_words.split("").sort == @word.split("").sort
       end
     end
   end
