31edaae01b6b02e4471a87f152d954b125db88f6
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-10 23:57:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,7 +5,7 @@ class Anagram
     @word = word
   end
   def match(some_words)
-    some_words.collect do |word|
+    some_words.find_all do |word|
       if word.split("").sort == @word.split("").sort
       end
     end
